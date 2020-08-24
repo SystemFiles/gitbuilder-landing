e5f3b0a7b02830cbd85f8c2bb52a31545fbd3102
Ben Sykes <ben@sykesdev.ca>
2020-08-24 13:47:40 -0400

    added features section

---
+++ app/components/FeatureCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from '../styles/FeatureCard.module.css'
+
+export class FeatureCard extends Component {
+	render() {
+		return (
+			<div className={styles.FeatureCard}>
+				<div>{this.props.icon}</div>
+				<h2>{this.props.title}</h2>
+				<p>{this.props.children}</p>
+			</div>
+		)
+	}
+}
+
+export default FeatureCard
--- app/pages/index.js
+++ app/pages/index.js
@@ -2,8 +2,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Grid } from '@material-ui/core'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import BuildIcon from '@material-ui/icons/Build'
+import CodeIcon from '@material-ui/icons/Code'
+import SecurityIcon from '@material-ui/icons/Security'
 
 import Button from '../components/Button'
+import FeatureCard from '../components/FeatureCard'
 
 export default function Home() {
 	return (
@@ -54,6 +58,44 @@ export default function Home() {
 			<Grid className={styles.aboutSection} container>
 				<Grid item xs={12}>
 					<h1>About</h1>
+				</Grid>
+				<Grid item xs={12}>
+					<p>
+						This tool, with it's extensible templates library (<span
+							className={styles.hightlight}
+							style={{ marginRight: '5px' }}
+						>
+							hosted on GitHub
+						</span>), allows developers to get up and running very quickly and without all the boring
+						environment and repository setup. gitBuilder.io will start by asking you a couple of basic
+						questions about your project and then will give you a detailed breakdown of how it is building
+						your project the entire way through.
+					</p>
+				</Grid>
+				<Grid className={styles.aboutFeatures} container item xs={12}>
+					<h1>Features</h1>
+					<Grid item xs={12}>
+						<p>See why GitBuilder.io is worth using!</p>
+					</Grid>
+					<Grid item container xs={12}>
+						<Grid item xs={12} sm={4}>
+							<FeatureCard icon={<BuildIcon color='secondary' />} title='Easy to use'>
+								Installs with One command, works with one command and only requires you to log in once!
+							</FeatureCard>
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<FeatureCard icon={<CodeIcon color='primary' />} title='Open Source'>
+								This project and it's associated templates are all open source on GitHub. Make or
+								request any changes you would like to see!
+							</FeatureCard>
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<FeatureCard icon={<SecurityIcon color='inherit' />} title='Secure'>
+								Gitbuilder.io supports both Basic (user/pass) authentication as well as OAuth
+								authentication for secure project generation.
+							</FeatureCard>
+						</Grid>
+					</Grid>
 				</Grid>
 			</Grid>
 
